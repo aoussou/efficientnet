772547250da9c5cb8937f8d2aa5cd030dcdba81b
ceo <john@talisol.com>
2022-09-24 02:24:03 +0900

    training working

--- main.py
+++ main.py
@@ -8,6 +8,17 @@ from tensorflow.keras.applications import EfficientNetB0
 import tensorflow_datasets as tfds
 import matplotlib.pyplot as plt
 
+from tensorflow.python.client import device_lib
+print(device_lib.list_local_devices())
+print(tf.__version__)
+print("GPUs Available: ", tf.config.list_physical_devices('GPU'))
+print(tf.test.is_gpu_available())
+print(tf.config.list_physical_devices('GPU'))
+
+import os
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+# tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+
 model = EfficientNetB0(weights='imagenet')
 # model = EfficientNetB0(include_top=False, weights='imagenet')
 # model = EfficientNetB0(weights='imagenet', drop_connect_rate=0.4)
@@ -15,7 +26,7 @@ model = EfficientNetB0(weights='imagenet')
 # IMG_SIZE is determined by EfficientNet model choice
 IMG_SIZE = 224
 
-batch_size = 64
+batch_size = 32
 
 dataset_name = "stanford_dogs"
 (ds_train, ds_test), ds_info = tfds.load(
@@ -28,23 +39,25 @@ ds_train = ds_train.map(lambda image, label: (tf.image.resize(image, size), labe
 ds_test = ds_test.map(lambda image, label: (tf.image.resize(image, size), label))
 
 
+# def format_label(label):
+#     string_label = label_info.int2str(label)
+#     return string_label.split("-")[1]
 
 
-def format_label(label):
-    string_label = label_info.int2str(label)
-    return string_label.split("-")[1]
+# label_info = ds_info.features["label"]
+# for i, (image, label) in enumerate(ds_train.take(9)):
+#     ax = plt.subplot(3, 3, i + 1)
+#     plt.imshow(image.numpy().astype("uint8"))
+#     plt.title("{}".format(format_label(label)))
+#     plt.axis("off")
+# plt.show()
 
 
-label_info = ds_info.features["label"]
-for i, (image, label) in enumerate(ds_train.take(9)):
-    ax = plt.subplot(3, 3, i + 1)
-    plt.imshow(image.numpy().astype("uint8"))
-    plt.title("{}".format(format_label(label)))
-    plt.axis("off")
-plt.show()
+
 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras import layers
+
 tf.config.experimental_run_functions_eagerly(True)
 img_augmentation = Sequential(
     [
@@ -56,11 +69,41 @@ img_augmentation = Sequential(
     name="img_augmentation",
 )
 
-for image, label in ds_train.take(1):
-    for i in range(9):
-        ax = plt.subplot(3, 3, i + 1)
-        aug_img = img_augmentation(tf.expand_dims(image, axis=0),training=True)
-        plt.imshow(aug_img[0].numpy().astype("uint8"))
-        plt.title("{}".format(format_label(label)))
-        plt.axis("off")
-plt.show()
+# for image, label in ds_train.take(1):
+#     for i in range(9):
+#         ax = plt.subplot(3, 3, i + 1)
+#         aug_img = img_augmentation(tf.expand_dims(image, axis=0), training=True)
+#         plt.imshow(aug_img[0].numpy().astype("uint8"))
+#         plt.title("{}".format(format_label(label)))
+#         plt.axis("off")
+# plt.show()
+
+
+# One-hot / categorical encoding
+def input_preprocess(image, label):
+    label = tf.one_hot(label, NUM_CLASSES)
+    return image, label
+
+
+ds_train = ds_train.map(
+    input_preprocess, num_parallel_calls=tf.data.AUTOTUNE
+)
+ds_train = ds_train.batch(batch_size=batch_size, drop_remainder=True)
+ds_train = ds_train.prefetch(tf.data.AUTOTUNE)
+
+ds_test = ds_test.map(input_preprocess)
+ds_test = ds_test.batch(batch_size=batch_size, drop_remainder=True)
+
+inputs = layers.Input(shape=(IMG_SIZE, IMG_SIZE, 3))
+x = img_augmentation(inputs)
+outputs = EfficientNetB0(include_top=True, weights=None, classes=NUM_CLASSES)(x)
+
+model = tf.keras.Model(inputs, outputs)
+model.compile(
+    optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"]
+)
+
+model.summary()
+
+epochs = 40  # @param {type: "slider", min:10, max:100}
+hist = model.fit(ds_train, epochs=epochs, validation_data=ds_test, verbose=2)
