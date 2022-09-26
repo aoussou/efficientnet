96b5e21e5222d840cd710ad5d789780057832c13
ceo <john@talisol.com>
2022-09-26 15:15:31 +0900

    successfully loaded tflite model and predicted

--- create_tflite_model.py
+++ create_tflite_model.py
@@ -1,12 +1,33 @@
 import tensorflow as tf
 from tensorflow.keras.models import load_model
-
+import numpy as np
 model = load_model('models/best_model.hdf5', compile=False)
 
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
 tflite_model = converter.convert()
 
+TFLITE_FILE_PATH = 'models/model.tflite'
 
 # Save the model.
-with open('models/model.tflite', 'wb') as f:
-  f.write(tflite_model)
+# with open('models/model.tflite', 'wb') as f:
+#     f.write(tflite_model)
+# f.close()
+
+interpreter = tf.lite.Interpreter(TFLITE_FILE_PATH)
+interpreter.allocate_tensors()
+
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+image_path = "data/screenshot.png"
+image = tf.io.decode_jpeg(tf.io.read_file(image_path))
+image_slice = image[:, :, 0:-1]
+gray_scale_image = tf.image.rgb_to_grayscale(image_slice)
+resized_image = tf.image.resize(gray_scale_image, size=(224, 224))
+input_data = tf.expand_dims(resized_image, 0)
+
+interpreter.set_tensor(input_details[0]['index'], input_data)
+
+interpreter.invoke()
+output_data = interpreter.get_tensor(output_details[0]['index'])
+print(np.argmax(output_data))
--- test_one_image.py
+++ test_one_image.py
@@ -1,20 +1,16 @@
-
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 import numpy as np
+
 model = load_model('models/best_model.hdf5', compile=False)
 
 image_path = "data/screenshot.png"
 
 image = tf.io.decode_jpeg(tf.io.read_file(image_path))
-image_slice = image[:,:,0:-1]
+image_slice = image[:, :, 0:-1]
 gray_scale_image = tf.image.rgb_to_grayscale(image_slice)
-
-# resized_image_slice = tf.expand_dims(image_slice,-1)
-
 resized_image = tf.image.resize(gray_scale_image, size=(224, 224))
-#
-#
-resized_image = tf.expand_dims(resized_image,0)
+
+resized_image = tf.expand_dims(resized_image, 0)
 pred = model.predict(resized_image)
 argmax = np.argmax(pred)
